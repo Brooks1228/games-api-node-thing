f0f1f817e0b212b8a6cfd0ff496a54ab8f63e95b
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2025-02-10 10:32:13 -0500

    `Refactored models/similar.js and views/game.ejs to fix issues and improve functionality.`

--- models/similar.js
+++ models/similar.js
@@ -1,31 +1,63 @@
-const db = require("../config/db.js");
+// const db = require("../config/db.js");
+
+// async function getAllSimilars(start = 0, limit = 50, gameId) {
+//   let where = "";
+//   let params = [];
+
+//   if (gameId) {
+//     where = " where g.game_id = ? ";
+//     params.push(gameId);
+//   }
+//   params.push(start.toString());
+//   params.push(limit.toString());
+
+//   const [rows] = await db.execute(
+//     `select g.*, g2.game_id as similar_game_id, g2.name as similar_game_name, g2.category as similar_category, g2.category as similar_category, g2.summary as similar_summary from games g
+//     inner join game_similar gs on gs.game_id = g.game_id
+//     inner join games g2 on gs.similar_game_id = g2.game_id
+//     left join game_covers gc on gc.game_id = g.game_id
+//     ${where} limit ?,?`,
+//     params
+//   );
+//   return rows;
+// }
+
+// async function getSimilarsById(gameId) {
+//   const [rows] = await db.execute(
+//     "Select * from game_similar gs inner join games g on g.game_id = gs.game_id where gs.similar_game_id = ?",
+//     [gameId]
+//   );
+//   return rows[0];
+// }
+
+// module.exports = { getAllSimilars, getSimilarsById };
+const db = require("../config/db");
 
 async function getAllSimilars(start = 0, limit = 50, gameId) {
   let where = "";
+  let join = "";
   let params = [];
-
   if (gameId) {
-    where = " where g.game_id = ? ";
+    join = "";
+    where = " where gs.game_id = ?";
     params.push(gameId);
   }
   params.push(start.toString());
   params.push(limit.toString());
 
   const [rows] = await db.execute(
-    `select g.*, g2.game_id as similar_game_id, g2.name as similar_game_name, g2.category as similar_category, g2.category as similar_category, g2.summary as similar_summary from games g
-    inner join game_similar gs on gs.game_id = g.game_id
-    inner join games g2 on gs.similar_game_id = g2.game_id ${where} limit ?,?`,
+    `select g.*, gc.url as cover_url from games g inner join game_similar gs on gs.similar_game_id = g.game_id left join game_covers gc on gc.game_id = g.game_id ${where} limit ?,?`,
     params
   );
   return rows;
 }
 
-async function getSimilarsById(gameId) {
+async function getSimilarById(gameId) {
   const [rows] = await db.execute(
     "Select * from game_similar gs inner join games g on g.game_id = gs.game_id where gs.similar_game_id = ?",
     [gameId]
   );
+
   return rows[0];
 }
-
-module.exports = { getAllSimilars, getSimilarsById };
+module.exports = { getAllSimilars, getSimilarById };
