0abfc15a7fc96abd518ab2ceb77a502cc1436906
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2025-02-10 09:30:22 -0500

    finished!

--- controllers/views/gamePageController.js
+++ controllers/views/gamePageController.js
@@ -1,11 +1,46 @@
 const axios = require("axios");
 const settings = require("../../config/settings");
 async function renderGamePage(req, res) {
-  let data = { game_id: req.params.id };
+  const response = await axios.get(
+    `${settings.ROOT}:${settings.PORT}/api/games/${req.params.id}`
+  );
+  let data = response.data;
+  const cover = await getGameDataFromEndpoint(req.params.id, "covers");
+
+  const genres = await getGameDataFromEndpoint(req.params.id, "genres");
+  const gameModes = await getGameDataFromEndpoint(req.params.id, "gameModes");
+  const platforms = await getGameDataFromEndpoint(req.params.id, "platforms");
+  const characters = await getGameDataFromEndpoint(req.params.id, "characters");
+  const websites = await getGameDataFromEndpoint(req.params.id, "websites");
+  const screenshots = await getGameDataFromEndpoint(
+    req.params.id,
+    "screenshots"
+  );
+  const similars = await getGameDataFromEndpoint(req.params.id, "similars");
+
+  data.genres = genres;
+  data.gameModes = gameModes;
+  data.platforms = platforms;
+  data.characters = characters;
+  data.websites = websites;
+  data.screenshots = screenshots;
+  data.similars = similars;
+
+  data.cover = cover[0] ? cover[0].url : "";
   res.render("game", {
-    title: "Game Page",
+    title: "CMP - IDGB: ",
     gameData: data,
   });
+}
+
+async function getGameDataFromEndpoint(gameid, endpoint) {
+  let values = await axios.get(
+    `${settings.ROOT}:${settings.PORT}/api/${endpoint}`,
+    {
+      params: { gameid: gameid },
+    }
+  );
+  return values.data;
 }
 
 module.exports = { renderGamePage };
